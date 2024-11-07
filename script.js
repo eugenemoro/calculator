7e373f8f57a90ab993e0f88b6cda08a5164b2e10
eugenemoro <29465704+eugenemoro@users.noreply.github.com>
2024-11-07 21:51:47 +0500

    feat: add logic and mouse click listener

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function divide(a, b) {
 let arg1 = 0;
 let arg2 = 0;
 let mathOperation = '';
+let previousButtonIsFunction = false;
 
 function operate(mathOperation, arg1, arg2) {
   switch (mathOperation) {
@@ -27,3 +28,54 @@ function operate(mathOperation, arg1, arg2) {
     default: break;
   }  
 }
+
+const buttons = document.getElementById('buttons');
+const display = document.getElementById('display');
+
+//REFACTOR: separate functions for input buttons and function buttons
+buttons.addEventListener('click', (e) =>{
+  if (e.target.classList.contains('btn-input')) {
+    if (display.value !== '0' && !previousButtonIsFunction) {
+      display.value += e.target.value;
+    } else {
+      display.value = e.target.value;
+      previousButtonIsFunction = false;
+    }
+  } else if (e.target.classList.contains('btn-function')) {
+    previousButtonIsFunction = true;
+    switch (e.target.id) {
+      case 'btn-divide': {
+        arg1 = Number(display.value);
+        mathOperation = 'divide';
+        break;
+      }
+      case 'btn-multiply': {
+        arg1 = Number(display.value);
+        mathOperation = 'multiply';
+        break;
+      }
+      case 'btn-add': {
+        arg1 = Number(display.value);
+        mathOperation = 'add';
+        break;
+      }
+      case 'btn-clear': {
+        arg1 = 0;
+        arg2 = 0;
+        mathOperation = '';
+        display.value = 0;
+        break;
+      }
+      case 'btn-subtract': {
+        arg1 = Number(display.value);
+        mathOperation = 'subtract';
+        break;
+      }
+      case 'btn-equal': {
+        arg2 = Number(display.value);
+        display.value = operate(mathOperation, arg1, arg2);
+        break;
+      }
+    }  
+  }
+});
